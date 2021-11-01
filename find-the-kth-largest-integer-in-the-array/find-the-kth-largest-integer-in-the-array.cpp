7679e7455fa35c15c1ad659298badb1f1484c38b
vkantoria <84955296+vkantoria@users.noreply.github.com>
2021-11-01 20:36:02 +0530

    Time: 648 ms (24.43%), Space: 335.4 MB (21.84%) - LeetHub

---
+++ find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    
+    static bool cmp(string a, string b)
+    {
+       return ((a.size() == b.size())?(a>b):a.size()>b.size());
+    }
+    
+    string kthLargestNumber(vector<string>& nums, int k) {
+        
+        sort(nums.begin(), nums.end(), cmp);
+       return nums[k-1];
+    }
+};
