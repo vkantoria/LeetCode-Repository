5bfb3db2d131d45d32f6f8715cbc08665d4b303d
vkantoria <84955296+vkantoria@users.noreply.github.com>
2021-11-01 10:58:43 +0530

    Time: 8 ms (54.51%), Space: 7.8 MB (17.64%) - LeetHub

---
+++ valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string curr = "";
+        for(int i = 0; i < s.size(); i++)
+            if(iswalnum(s[i]))
+                curr += s[i];
+        
+        transform(curr.begin(),curr.end(), curr.begin(), :: tolower);
+        int i = 0, j = curr.size() - 1;
+        while(i <= j)
+        {
+            if(curr[i] != curr[j])
+                return false;
+            i++; j--;
+        }
+        return true;   
+    }
+};
