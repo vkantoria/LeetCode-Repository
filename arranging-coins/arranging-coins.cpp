f53dc21bd7d04e3ddd644499e611dfa18bd14da7
vkantoria <84955296+vkantoria@users.noreply.github.com>
2021-11-05 11:19:28 +0530

    Time: 4 ms (68.61%), Space: 6 MB (26.19%) - LeetHub

--- arranging-coins/arranging-coins.cpp
+++ arranging-coins/arranging-coins.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
+    
+    bool check(long long curr, long long n)
+    {
+       if(curr<=n)
+           return true;
+        else
+            return false;
+    }
+    
     int arrangeCoins(int n) {
-       
-        long left = 0;
-        long right = n;
-        long mid , curr;
+       long long left = 1;
+       long long right = n;
+       long long mid, curr, ans;
         
         while(left <= right)
         {
             mid = left + (right - left)/2;
             curr = mid * (mid + 1)/2;
-            
-            if(n == curr) return (int)mid;
-            else if( n < curr) right = mid - 1;
-            else left = mid + 1;
-                
+             if(curr == n)
+                return mid;
+            if(check(curr, n))
+                ans = mid;
+            if( n<curr) right = mid - 1;
+            else left = mid + 1;      
         }
-        return int(right);
+        return int(ans);
         
         
     }
